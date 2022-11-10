d725a7d760268878880ef71994d444b56211012e
Paras Arora <paras.arora@berkeley.edu>
2022-11-10 23:57:00 +0100

    finished landing page and animals view

--- zoo-app/zoo/main.py
+++ zoo-app/zoo/main.py
@@ -21,3 +21,25 @@ def index():
         ),
     ]
     return render_template("main/index.html", posts=posts)
+
+@bp.route("/animals")
+def animals():
+    polar_bear = model.Animal(1, "Polar Bear", "Ursus", "A very, very scary animal!", "https://source.unsplash.com/qQWV91TTBrE")
+    brown_bear = model.Animal(2, "Brown Bear", "Ursus", "A very, very scary animal!", "https://source.unsplash.com/y421kXlUOQk")
+    tiger = model.Animal(3, "Tiger", "Ursus", "A very, very scary animal!", "https://source.unsplash.com/opnyo_F5l3o")
+    snake = model.Animal(4, "Snake", "Ursus", "A very, very scary animal!", "https://source.unsplash.com/o1bdnLXC5NM")
+    toucan = model.Animal(5, "Toucan", "Ursus", "A very, very scary animal!", "https://source.unsplash.com/cfIcTiopin4")
+    shark = model.Animal(6, "Shark", "Ursus", "A very, very scary animal!", "https://source.unsplash.com/GBDkr3k96DE")
+
+    animals = [polar_bear, brown_bear, tiger, snake, toucan, shark]
+
+    return render_template("main/animals.html", animals=animals)
+
+@bp.route("/faq")
+def faq():
+    item1 = model.FaqItem(1, "Are pets allowed at the park?", "No, unfortunately pets are not allowed for various saftey reasons.")
+    item2 = model.FaqItem(2, "What are the hours of the park?", "The park hours are 9AM - 8PM on weekdays and 8AM - 10PM on weekends.")
+
+    faq_items = [item1, item2]
+
+    return render_template("", faq_items=faq_items)
--- zoo-app/zoo/model.py
+++ zoo-app/zoo/model.py
@@ -10,3 +10,17 @@ class User:
         self.user_id = user_id
         self.email = email
         self.name = name
+
+class Animal:
+    def __init__(self, animal_id, name, genus, description=None, img_src=None):
+        self.animal_id = animal_id
+        self.name = name
+        self.genus = genus
+        self.description = description
+        self.img_src = img_src
+
+class FaqItem:
+    def __init__(self, faq_id, title, description):
+        self.faq_id = faq_id
+        self.title = title
+        self.description = description
